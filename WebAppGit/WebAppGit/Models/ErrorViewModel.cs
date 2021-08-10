4cb0e86cf410e8f178c15b5791d8772a0d1ffaf6
Mauro Cristóvão <cristovao.mauro@hotmail.com>
2021-08-10 21:34:09 +0100

    Primeiro teste

---
+++ WebAppGit/WebAppGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
